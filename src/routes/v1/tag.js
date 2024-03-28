83f84201507dfc3c3533966537b2c36225c44a82
Horoli <sunha321@gmail.com>
2024-03-28 20:28:15 +0900

    update

--- src/routes/v1/tag.js
+++ src/routes/v1/tag.js
@@ -44,20 +44,40 @@ module.exports = {
     async handler(req, rep) {
       const { type } = req.params;
 
-      console.log(type);
-
       const tagCol = await MongoDB.getCollection("tag");
 
-      const getTags = await Document.getDatas({
-        collection: tagCol,
-        query: {
-          type: parseInt(type),
-          "status.enable": true,
-        },
-        queryOptions: {
-          projection: { _id: 0, status: 0 },
-        },
-      });
+      const typeNullCheck = !type;
+
+      let getTags;
+
+      switch (typeNullCheck) {
+        case true: {
+          getTags = await Document.getDatas({
+            collection: tagCol,
+            query: {
+              "status.enable": true,
+            },
+            queryOptions: {
+              projection: { _id: 0, status: 0 },
+            },
+          });
+          break;
+        }
+        case false: {
+          const convertType = parseInt(type);
+          getTags = await Document.getDatas({
+            collection: tagCol,
+            query: {
+              type: convertType,
+              "status.enable": true,
+            },
+            queryOptions: {
+              projection: { _id: 0, status: 0 },
+            },
+          });
+          break;
+        }
+      }
 
       return new GeneralResponse({
         statusCode: 200,
--- src/routes/v1/third_party.js
+++ src/routes/v1/third_party.js
@@ -2,6 +2,7 @@ const Document = require("@Utility/document");
 const Constants = require("@Utility/constants");
 const MongoDB = require("@Utility/mongodb");
 const GeneralResponse = require("@Utility/general_response");
+const Utility = require("@Utility/index");
 
 module.exports = {
   "POST /:manager": {
@@ -19,30 +20,53 @@ module.exports = {
         tags,
       } = req.body;
 
+      const convertType = parseInt(type);
+
       const thirdPartyCol = await MongoDB.getCollection("thirdParty");
+      const imageCol = await MongoDB.getCollection("image");
 
       // TODO : 입력된 tags가 tagCol에 있는지 확인
 
       // TODO : type이 constants에 포함되어있는지 확인
 
+      // TODO : type
+
+      console.log("aaa");
+
       const getData = await Document.postValidation({
         collection: thirdPartyCol,
         query: {
-          type: type,
+          type: convertType,
           label: label,
         },
       });
 
+      let imageId = null;
+
+      if (!!thumbnail) {
+        imageId = Utility.UUID(true);
+
+        await imageCol.insertOne({
+          id: imageId,
+          contents: thumbnail,
+          type: "base64String",
+          status: {
+            created: new Date(),
+            enable: true,
+          },
+        });
+      }
+
       const insertData = {
-        type: type,
+        type: convertType,
         label: label,
         description: {
           main: mainDescription,
-          sub: subDescription,
+          sub: subDescription ?? null,
         },
         images: {
-          thumbnail: thumbnail,
-          info: infoImages,
+          thumbnail: imageId,
+          info: infoImages ?? null,
         },
         url: {
           main: mainUrl,
@@ -56,7 +80,7 @@ module.exports = {
           click: 0,
         },
       };
-
+      // console.log(insertData);
       const insertResult = await thirdPartyCol.insertOne(insertData);
 
       return new GeneralResponse({
@@ -72,22 +96,43 @@ module.exports = {
     async handler(req, rep) {
       const { tag } = req.params;
 
-      console.log("tag", tag);
-
       const thirdPartyCol = await MongoDB.getCollection("thirdParty");
 
-      const getThirdParties = await Document.getDatas({
-        collection: thirdPartyCol,
-        query: {
-          tags: {
-            $in: [tag],
-          },
-          "status.enable": true,
-        },
-        queryOptions: {
-          projection: { _id: 0, status: 0 },
-        },
-      });
+      const tagNullCheck = !tag;
+
+      console.log(tagNullCheck);
+
+      let getThirdParties;
+
+      switch (tagNullCheck) {
+        case true: {
+          getThirdParties = await Document.getDatas({
+            collection: thirdPartyCol,
+            query: {
+              "status.enable": true,
+            },
+            queryOptions: {
+              projection: { _id: 0, status: 0 },
+            },
+          });
+          break;
+        }
+        case false: {
+          getThirdParties = await Document.getDatas({
+            collection: thirdPartyCol,
+            query: {
+              tags: {
+                $in: [tag],
+              },
+              "status.enable": true,
+            },
+            queryOptions: {
+              projection: { _id: 0, status: 0 },
+            },
+          });
+          break;
+        }
+      }
 
       console.log(getThirdParties);
 
