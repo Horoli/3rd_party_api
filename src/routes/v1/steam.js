beb17f8e001f6c5df83c9d81278800d71f4a5afd
Horoli <sunha321@gmail.com>
2024-05-02 18:41:58 +0900

    update

--- src/routes/v1/steam.js
+++ src/routes/v1/steam.js
@@ -18,11 +18,11 @@ module.exports = {
 
       //   console.log(await SteamAPIInstance.sharedInstance.getGameDetails(238960));
 
-      //   console.log(await steamApi.getGamePlayerCounts(238960));
+      console.log(await steamApi.getGamePlayerCounts(238960));
 
       //   const get = await steamApi.getGameNews(poeId);
       //   const get = await steamApi.getGameAchievementsPercentages(poeId);
-      const get = await steamApi.getUserStats(userId, poeId);
+      //   const get = await steamApi.getUserStats(userId, poeId);
       //   const get = await steamApi.getServers();
       console.log(get.length);
 
