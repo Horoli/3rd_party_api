84f5070539cd8ae45155423e47511963a02e1a48
Horoli <sunha321@gmail.com>
2024-08-26 12:48:49 +0900

    update

--- src/routes/v1/poe_ninja.js
+++ src/routes/v1/poe_ninja.js
@@ -1,13 +1,31 @@
 const PoeNinja = require("@Utility/poe_ninja");
+const Axios = require("axios");
 // const { NinjaAPI } = require("poe-api-manager");
 module.exports = {
   "GET /": {
     middlewares: [],
-    async handler(req, res) {
-      const asd = await PoeNinja.get("Settlers");
-      return {
-        data: asd,
-      };
+    async handler(req, rep) {
+      // const asd = await PoeNinja.get("Settlers");
+
+      // const zxc = await Axios.post("http://172.16.0.6:7100/image", {
+      //   url: "aa",
+      // });
+
+      const result = await Axios({
+        method: "post",
+        url: "http://172.16.0.6:7100/image",
+        data: {
+          url: "https://web.poecdn.com/gen/image/WzI4LDE0LHsiZiI6IjJESXRlbXMvTWFwcy9BdGxhczJNYXBzL05ldy9VYmVyTWFsZm9ybWF0aW9uIiwidyI6MSwiaCI6MSwic2NhbGUiOjEsIm1uIjoyMSwibXQiOjE3LCJtZCI6dHJ1ZX1d/89b5c5eec9/UberMalformation.png",
+        },
+        responseType: "arraybuffer",
+      });
+      console.log(result);
+
+      rep.type("image/png").send(result.data);
+
+      // return {
+      //   data: result.data,
+      // };
     },
   },
 };
